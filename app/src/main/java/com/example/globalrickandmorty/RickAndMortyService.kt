ecdf229501fb967b11f76a152e7410359d6cbc35
ticiano.filho <ticiano.filho@mv.com.br>
2024-07-16 11:26:08 -0300

    test: rest api communication using retrofit2 and moshi for response parsing

--- app/src/main/java/com/example/globalrickandmorty/RickAndMortyService.kt
+++ app/src/main/java/com/example/globalrickandmorty/RickAndMortyService.kt
@@ -1,4 +1,9 @@
 package com.example.globalrickandmorty
 
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface RickAndMortyService {
+    @GET(value = "character/2")
+    fun getCharacterById(): Call<Any>
 }
